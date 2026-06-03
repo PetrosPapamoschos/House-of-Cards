95fce4b8e58dcf2f3fc50dd6eaf9287363ac96b9
Stergios P <stepetalo@protonmail.com>
2026-06-03 16:48:11 +0300

    ..

--- House.java
+++ House.java
@@ -5,11 +5,16 @@ public class House {
     private int houseNumber;
     private int houseScore;
     private List<Card> cardsInHouse;
+    private boolean isHouseClosed;
 
     public House(int houseNumber) {
         this.houseNumber = houseNumber;
         this.houseScore = 0;
-        this.cardsInHouse = new ArrayList<>();
+        this.cardsInHouse = new ArrayList<Card>();
+        this.isHouseClosed = false;
+    }
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
     }
     public void setHouseScore(int houseScore) {
         this.houseScore = houseScore;
@@ -18,21 +23,30 @@ public class House {
     public int getHouseScore() {
         return houseScore;
     }
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+    public boolean getIsHouseClosed() {
+        return isHouseClosed;
+    }
+
     public int calculateHouseScore(int houseNumber, int houseScore, ArrayList<Card> cardsInHouse) { 
         for (Card card : cardsInHouse) {
             houseScore += card.getValue();
         }
         return houseScore;
     }
+
     public void addCardToHouse(Card card) {
         cardsInHouse.add(card);
     }
-    public void setHouseNumber(int houseNumber) {
-        this.houseNumber = houseNumber;
-    }
+
 //    public void resetHouseScore() {
 //        this.houseScore = 0;
 //    }
+    public void closeHouse(int houseNumber) {
+        this.isHouseClosed = true;
+    }
 
     @Override
     public String toString() {
