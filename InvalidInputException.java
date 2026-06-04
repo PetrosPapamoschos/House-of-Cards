b7e1e34f6b83e26963a4a5b87025d790368521e9
Stergios P <stepetalo@protonmail.com>
2026-06-04 14:36:11 +0300

    ..

---
+++ InvalidInputException.java
@@ -0,0 +1,9 @@
+public class InvalidInputException extends Exceptions{
+
+    public InvalidInputException() {
+        super();
+    }
+    public InvalidInputException(String m) {
+        super(m);
+    }
+}
